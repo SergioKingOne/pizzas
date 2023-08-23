28490d199b93faa604b41cfc308e1338fd7f1d92
Sergio <sergiorobayorr@gmail.com>
2023-08-23 16:28:38 -0500

    Added props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,20 +49,80 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
-      <h1>Hello React!</h1>
-      <Pizza />
+    <div className="container header">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
-function Pizza() {
+function Header() {
+  // const style = { color: "red", fontSize: "32px", textTransform: "uppercase" };
+  const style = {};
+
   return (
-    <div>
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    <header className="header footer">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredient="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Fungi"
+        ingredients="Tomato, mushroom"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      ></Pizza>
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name}></img>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredient}</p>
+        <span>{+props.price + 3}</span>
+      </div>
     </div>
   );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) {
+  //   alert("We're currently open!");
+  // } else {
+  //   alert("Sorry we close");
+  // }
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleString()}We're currently open
+    </footer>
+  );
+
+  //return React.createElement("footer", null, "We're currently helping");
 }
 
 // React v18
